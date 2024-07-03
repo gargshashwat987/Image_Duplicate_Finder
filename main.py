56679ffdb9b5a952c1df34fab0783808771415d2
Samridhi <samridhi.agarwal001122@gmail.com>
2024-07-03 18:35:42 +0530

    main.py

--- main.py
+++ main.py
@@ -3,17 +3,11 @@ import imagehash
 import os
 import openpyxl
 from tkinter import filedialog
-from PIL import Image
+from PIL import Image, ImageTk
 from collections import defaultdict
 from tkinter import ttk
-from PIL import Image
-from PIL import UnidentifiedImageError 
+from PIL import UnidentifiedImageError
 from datetime import datetime
-from PIL import Image, ImageTk
-
-
- 
-
 
 def find_duplicate_images(folder_path, single_image_path):
     hash_dict = defaultdict(list)
@@ -31,35 +25,28 @@ def find_duplicate_images(folder_path, single_image_path):
     duplicates = hash_dict[single_image_hash] if single_image_path else hash_dict.get(single_image_hash, [])
     return duplicate_path, duplicates
 
- 
-
 def save_to_excel(file_path, duplicates):
     wb = openpyxl.Workbook()
     ws = wb.active
     ws.append(['Index', 'Path'])
     for i, duplicate_path in enumerate(duplicates, start=1):
         ws.append([i, duplicate_path])
-    # Include a timestamp in the file name
     date = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     filename = f"Duplicates_{date}.xlsx"
     full_path = os.path.join(file_path, filename)
     wb.save(full_path)
-
- 
 
 def browse_folder():
     global folder_path
     folder_path = filedialog.askdirectory()
     folder_label_var.set(folder_path)
 
- 
-
 def browse_single_image():
     global single_image_path
-    single_image_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg *.jpeg *.png *.gif *.bmp *.tiff *.ico")])
+    single_image_path = filedialog.askopenfilename(
+        filetypes=[("Image Files", "*.jpg *.jpeg *.png *.gif *.bmp *.tiff *.ico *.jfif")])
     single_image_label_var.set(single_image_path)
 
- 
 def calculate_dhash(image_path):
     try:
         with Image.open(image_path) as img:
@@ -86,59 +73,67 @@ def process_image():
     else:
         result_text.set("Please select a folder and a single image before processing.")
 
-
-# Create the main window
 root = tk.Tk()
 root.title("Duplicate Image Finder")
-root.configure(bg='white')  # Set the background color to red 
+
+logo_image = Image.open(r"D:\User Files\Downloads\path_infotech_ltd_logo.png")
+logo_image = logo_image.resize((70, 70), Image.BOX)  # Adjust the size as needed
+logo_photo = ImageTk.PhotoImage(logo_image)
+
+logo_label = tk.Label(root, image=logo_photo, bg='white')
+logo_label.photo = logo_photo
+logo_label.grid(row=0, column=0, columnspan=5,padx=5, pady=10, sticky="nw")
+
+root.configure(bg='white')
 
 style = ttk.Style()
 style.configure("Colorful.TButton",
                 background="black",
                 foreground="black",
-                padding=(5, 2)) 
+                padding=(5, 2))
 style.map("Colorful.TButton",
           background=[('active', 'green')],
           foreground=[('active', 'blue')])
 style.configure("Treeview",
                 font=("Helvetica", 12),
-                background="white",  # Set the background of the Treeview to white
-                fieldbackground="white",  # Set the background of the fields in Treeview to white
-                foreground="black")  # Set the foreground (text color) to black
+                background="white",
+                fieldbackground="white",
+                foreground="black")
 
-frame = tk.Frame(root, bg='white')  # Set the frame's background color to red
-frame.pack(padx=10, pady=10)
+frame = tk.Frame(root, bg='white')
+frame.grid(row=0,column=5,padx=50, pady=10,sticky='n')
 
-title_label = tk.Label(frame, text="Duplicate Image Finder", font=("Helvetica", 20), bg='white', fg='red')  # Set the text color to white
+title_label = tk.Label(frame, text="Duplicate Image Finder", font=("Helvetica", 20), bg='white',
+                       fg='red')
+title_label.grid(row=0, column=5,padx=5, pady=10,sticky='n')
 title_label.pack()
 
- 
 folder_label_var = tk.StringVar()
 folder_label_var.set("Select a folder")
-folder_label = tk.Label(frame, textvariable=folder_label_var, bg='white', fg='red', font=("Helvetica", 10))  # Set the text color to white
-folder_label.pack()
+folder_label = tk.Label(frame, textvariable=folder_label_var, bg='white', fg='red',
+                        font=("Helvetica", 8))
+folder_label.pack(pady=5, padx=5)
 
- 
 browse_folder_button = ttk.Button(frame, text="Browse Folder", command=browse_folder, style="Colorful.TButton")
 browse_folder_button.pack(pady=10)
 
- 
 single_image_label_var = tk.StringVar()
 single_image_label_var.set("Select a single image")
-single_image_label = tk.Label(frame, textvariable=single_image_label_var, bg='white', fg='red', font=("Helvetica", 10))  # Set the text color to white
-single_image_label.pack()
+single_image_label = tk.Label(frame, textvariable=single_image_label_var, bg='white', fg='red',
+                              font=("Helvetica", 8))
+single_image_label.pack(pady=5, padx=5)
 
-browse_single_image_button = ttk.Button(frame, text="Select Single Image",command=browse_single_image, style="Colorful.TButton")
+browse_single_image_button = ttk.Button(frame, text="Select Single Image", command=browse_single_image,
+                                        style="Colorful.TButton")
 browse_single_image_button.pack(pady=10)
 
-
 process_button = ttk.Button(frame, text="Process Image", command=process_image, style="Colorful.TButton")
-process_button.pack(pady=20, padx=0)
+process_button.pack(pady=25, padx=25)
 
- 
 result_text = tk.StringVar()
 result_text.set("")
-result_label = tk.Label(frame, textvariable=result_text, bg='white', fg='red', font=("Helvetica", 14))  # Set the text color to white
+result_label = tk.Label(frame, textvariable=result_text, bg='white', fg='red',
+                        font=("Helvetica", 14))
 result_label.pack()
 result_display = ttk.Treeview(frame, columns=('Index', 'Path'), show='headings', height=15)
 result_display.heading('Index', text='Index')
